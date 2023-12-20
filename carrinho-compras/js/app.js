9628ff2ad6c6098598814ac2624d7172e8eeb4b8
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-20 15:37:02 -0300

    Calculando o valor do produto

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,7 +1,10 @@
 function adicionar() {
-    // pegar valores
-    let produtoSelecionado = document.getElementById("produto");
-    let quantidade = document.getElementById("quantidade");
-    alert(produtoSelecionado.value)
-    alert(quantidade.value)
+  let produto = document.getElementById("produto").value;
+  let nome = produto.split("-")[0];
+  let valorUni = produto.split("R$")[1];
+  let quantidade = document.getElementById("quantidade").value;
+  let subtotal = valorUni * quantidade;
+
+  alert(nome);
+  alert(subtotal);
 }
