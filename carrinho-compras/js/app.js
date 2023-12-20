ab3b024d98be5ddc8af22a095b546305de368be5
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-20 15:22:37 -0300

    Obtendo o produto adicionado

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,7 @@
+function adicionar() {
+    // pegar valores
+    let produtoSelecionado = document.getElementById("produto");
+    let quantidade = document.getElementById("quantidade");
+    alert(produtoSelecionado.value)
+    alert(quantidade.value)
+}
