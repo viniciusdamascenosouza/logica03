353b5bd9c7c5080e85b4edfe89d31588733f76a0
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-21 15:25:27 -0300

    Obtendo ingresso escolhido

---
+++ ingresso/js/app.js
@@ -0,0 +1,7 @@
+function comprar() {
+  let tipo = document.querySelector("#tipo-ingresso");
+  let quantidade = document.querySelector("#qtd");
+
+  alert(tipo.value);
+  alert(quantidade.value);
+}
