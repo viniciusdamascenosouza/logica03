bdf7723d6dfdf804feef97aff21974d29e95a307
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-21 15:43:28 -0300

    Realizando compra para tipo pista

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,7 +1,20 @@
 function comprar() {
   let tipo = document.querySelector("#tipo-ingresso");
-  let quantidade = document.querySelector("#qtd");
+  let quantidade = parseInt(document.querySelector("#qtd").value);
 
-  alert(tipo.value);
-  alert(quantidade.value);
+  if (tipo.value == "pista") {
+    comprarPista(quantidade);
+  }
+}
+
+function comprarPista(quantidade) {
+  let quantidadePista = parseInt(document.querySelector("#qtd-pista").textContent);
+
+  if (quantidade > quantidadePista) {
+    alert("Quantidade indisponível!");
+  } else {
+    quantidadePista = quantidadePista - quantidade;
+    document.querySelector("#qtd-pista").textContent = quantidadePista;
+    alert("Compra realizada com sucesso!");
+  }
 }
