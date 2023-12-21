c80a1be4b69c1d8933ecad7d25e93aa2fb97fbbc
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-21 15:59:38 -0300

    desafios

---
+++ desafios/conversao-string.js
@@ -0,0 +1,8 @@
+function converterParaInteiro(valorString) {
+  return parseInt(valorString);
+}
+
+// Exemplo de uso
+let valorString = "42";
+let valorInteiro = converterParaInteiro(valorString);
+console.log(valorInteiro); // Saída esperada: 42
