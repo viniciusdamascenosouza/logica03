1ddc13b5f18c78ddabf16c766296938052b851ec
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-12-21 14:47:08 -0300

    Total e limpando campo de qunatidade

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,14 +1,26 @@
+let totalGeral = 0;
+document.getElementById("lista-produtos").innerHTML = "";
+let campoTotal = document.querySelector("#valor-total");
+campoTotal.innerHTML = "";
+
 function adicionar() {
   let produto = document.getElementById("produto").value;
   let nome = produto.split("-")[0];
   let valorUni = produto.split("R$")[1];
   let quantidade = document.getElementById("quantidade").value;
-  let subtotal = valorUni * quantidade;
+  let preco = valorUni * quantidade;
 
   let carrinho = document.getElementById("lista-produtos");
-  carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
-                            <section class="carrinho__produtos__produto">
-                                <span class="texto-azul">${quantidade}x</span> ${nome}<span class="texto-azul"> R$${valorUni}</span>
-                            </section>
-                        </section>`;
+  carrinho.innerHTML =
+    carrinho.innerHTML +
+    `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${nome}<span class="texto-azul"> R$${valorUni}</span>
+        </section>
+    </section>`;
+
+  document.getElementById("quantidade").value = "";
+  totalGeral = totalGeral + preco;
+  campoTotal.textContent = `R$ ${totalGeral}`;
+  
 }
